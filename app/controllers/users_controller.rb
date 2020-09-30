79956041f4281655f9043e318439846450e12516
愿山聖 <asjstksmr@gmail.com>
2020-09-30 19:12:26 +0900

    [add]index_page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,12 @@ class UsersController < ApplicationController
 
 	def index
 		@users = User.all
+		@book = Book.new
 	end
 
 	def show
 		@user = User.find(params[:id])
 		@books = Book.where(user_id: @user)
-		p @books
 		@book = Book.new
 	end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#top'
   get 'about' => 'homes#about'
-  resources :books, only: [:index, :create, :edit, :update, :destroy]
+  resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
   resources :users, only: [:index, :show, :edit, :update]
 end
