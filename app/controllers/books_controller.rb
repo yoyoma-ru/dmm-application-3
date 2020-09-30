78731b4e966b9ac5f41213aae43f49ab60629643
愿山聖 <asjstksmr@gmail.com>
2020-09-30 16:56:27 +0900

    [add]edit_page/destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,12 +21,25 @@ class BooksController < ApplicationController
 	end
 
 	def edit
+		@book = Book.find(params[:id])
 	end
 
 	def update
+		book = Book.find(params[:id])
+		if book.update(book_params)
+			redirect_to book_path(book), notice: "You have updated book successfully"
+		else
+			render :edit
+		end
 	end
 
 	def destroy
+		book = Book.find(params[:id])
+		if book.destroy
+			redirect_to books_path
+		else
+			render :show
+		end
 	end
 
 	private
