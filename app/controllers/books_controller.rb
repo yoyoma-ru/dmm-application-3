275c0ab6a76530cff9180b28e14a6a18f730dea4
愿山聖 <asjstksmr@gmail.com>
2020-09-30 15:02:00 +0900

    [add]index_page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,34 @@
 class BooksController < ApplicationController
+
+	def index
+		@books = Book.all
+		@book = Book.new
+	end
+
+	def show
+	end
+
+	def create
+		book = Book.new(book_params)
+		book.user_id = current_user.id
+		if book.save
+			redirect_to book_path(book), notice: "You have created book successfully"
+		else
+			render :index
+		end
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+	def book_params
+		params.require(:book).permit(:title, :body)
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_29_073352) do
+ActiveRecord::Schema.define(version: 2020_09_30_053851) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
